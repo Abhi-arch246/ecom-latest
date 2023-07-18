cd05ff3e71131f3860f62761b94e1526c1871c7e
Abhi-arch246 <abhishekreddy235@gmail.com>
2023-07-18 22:47:59 +0530

    Project init commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
