53e1851e6f9636420b462f0916da4fdd81346e51
Abhi-arch246 <abhishekreddy235@gmail.com>
2023-09-08 23:26:57 +0530

    Deploy code

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,9 @@ app.get("/api/paypal/config", (req, res) =>
 if (process.env.NODE_ENV === "production") {
   app.use("/", express.static("../frontend/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "..", "frontend", "build", "index.html")
+    );
   });
 } else {
   app.get("/", (req, res) => {
