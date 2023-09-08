450e2e6ae59ae818af6834256acfd1fd3d89353a
Abhi-arch246 <abhishekreddy235@gmail.com>
2023-09-08 23:36:42 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+    build: {
+      outDir: "../dist",
+    },
     proxy: {
       "/api": {
         target: "http://localhost:5050/",
