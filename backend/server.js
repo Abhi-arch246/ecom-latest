6b8aac631f17050235d754331a441edf518f3c02
Abhi-arch246 <abhishekreddy235@gmail.com>
2023-09-08 23:15:28 +0530

    Node env prod

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,9 @@ app.get("/api/paypal/config", (req, res) =>
 );
 
 if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use("/", express.static("../frontend/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
   });
 } else {
   app.get("/", (req, res) => {
