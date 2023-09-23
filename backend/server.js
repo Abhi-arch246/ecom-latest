91a72e1b44b6c0328166dc8b4440b2f151a32a0f
Abhi-arch246 <abhishekreddy235@gmail.com>
2023-09-23 12:19:12 +0530

    Render deploy changes

--- backend/server.js
+++ backend/server.js
@@ -22,12 +22,11 @@ app.get("/api/paypal/config", (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
+__dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("../frontend/build"));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "..", "frontend", "build", "index.html")
-    );
+    res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
